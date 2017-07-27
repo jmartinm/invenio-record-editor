4ace7bb361bef6b2643427657e88a08450442e4c
Javier Martin Montull <javier.martin.montull@cern.ch>
2017-07-27 13:57:12 +0200

    record-editor bump to 0.7.0

--- invenio_record_editor/bundles.py
+++ invenio_record_editor/bundles.py
@@ -35,6 +35,6 @@ js = NpmBundle(
     depends=("node_modules/record-editor/dist/*.js"),
     output="gen/record-editor.%(version)s.js",
     npm={
-        "record-editor": "~0.6.0"
+        "record-editor": "^0.7.0"
     }
 )
